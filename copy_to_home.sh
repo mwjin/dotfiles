fa4927eba53f2e4e00776cc0fd677234e1168646
Minwoo Jeong <mwjeong.sci@gmail.com>
2020-11-19 15:57:18 +0900

    Create copy_to_home.sh

---
+++ copy_to_home.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# This script is to copy the dotfiles into the HOME directory automatically.
+
+# Get directories of dotfiles
+dirs=(`ls -l | grep "^d" | awk '{print $9}'`);
+
+# Copy dotfiles into the HOME directory
+for dir in ${dirs[@]}
+do
+	dotfiles=(`ls -A $dir`);
+	
+	for dotfile in ${dotfiles[@]}
+	do
+		dotfile_path="$dir/$dotfile";
+		cmd="cp -r $dotfile_path $HOME/.";
+		echo "[CMD] $cmd";
+		$cmd;
+	done;
+done;
+
+# Clone remote repositories of git submodules for the dotfiles
+submod_paths=(`cat .gitmodules | grep path | awk '{split($0, a, " = "); print a[2]}'`);
+submod_urls=(`cat .gitmodules | grep url | awk '{split($0, a, " = "); print a[2]}'`);
+
+for ((i=0;i<${#submod_paths[@]};i++));
+do
+	submod_path="$HOME/${submod_paths[i]#*/}";  # Make an absolute local path of the submodule
+	submod_url="${submod_urls[i]}";
+	cmd="git clone $submod_url $submod_path";
+	echo "[CMD] $cmd";
+	$cmd;
+done;
+
