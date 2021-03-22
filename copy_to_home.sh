ad8084fb64473cb1fa5ed9d918cbb834fa9c0ef3
Minwoo Jeong <mwjeong.sci@gmail.com>
2021-03-22 20:29:37 +0900

    Modify the script to be able to copy one specific directory

--- copy_to_home.sh
+++ copy_to_home.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
+# Usage: ./copy_to_home.sh [Directory name] ([]: Optional)
 # This script is to copy the dotfiles into the HOME directory automatically.
 
-# Get directories of dotfiles
-dirs=(`ls -l | grep "^d" | awk '{print $9}'`);
-
 # Copy dotfiles into the HOME directory
-for dir in ${dirs[@]}
-do
+if [ "$#" -eq 1 ]
+then
+    # One specific directory
+    dir=$1
     dotfiles=(`ls -A $dir`);
     
     for dotfile in ${dotfiles[@]}
@@ -16,18 +16,47 @@ do
         echo "[CMD] $cmd";
         $cmd;
     done;
-done;
+else
+    # No arguments -> Copy all directories
+    # Get directories of dotfiles
+    dirs=(`ls -l | grep "^d" | awk '{print $9}'`);
+    for dir in ${dirs[@]}
+    do
+        dotfiles=(`ls -A $dir`);
+    
+        for dotfile in ${dotfiles[@]}
+        do
+            dotfile_path="$dir/$dotfile";
+            cmd="cp -r $dotfile_path $HOME/.";
+            echo "[CMD] $cmd";
+            $cmd;
+        done;
+    done;
+fi;
 
 # Clone remote repositories of git submodules for the dotfiles
 submod_paths=(`cat .gitmodules | grep path | awk '{split($0, a, " = "); print a[2]}'`);
 submod_urls=(`cat .gitmodules | grep url | awk '{split($0, a, " = "); print a[2]}'`);
 
-for ((i=0;i<${#submod_paths[@]};i++));
+for ((i=0;i<${#submod_paths[@]};i++))
 do
     submod_path="$HOME/${submod_paths[i]#*/}";  # Make an absolute local path of the submodule
     submod_url="${submod_urls[i]}";
-    cmd="git clone $submod_url $submod_path";
-    echo "[CMD] $cmd";
-    $cmd;
+    
+    do_clone=true
+    if [ "$#" -eq 1 ]
+    then
+        if [[ $submod_path != *$1* ]]
+        then
+            do_clone=false
+        fi
+    fi
+
+    if [ $do_clone = true ]
+    then
+        cmd="git clone $submod_url $submod_path";
+        echo "[CMD] $cmd";
+        $cmd;
+    fi
 done;
 
